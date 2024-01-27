4d7318aa184f4b1fcb41fdddb9b883669064235e
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2024-01-26 23:47:28 -0800

    header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./pages/Chat";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Chat />} />
       </Routes>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header">
+        <img />
+      </div>
+    </header>
+  );
+}

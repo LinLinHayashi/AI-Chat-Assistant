aefe31bf3de839c0372304198774c34b8b33d381
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2024-01-28 15:41:14 -0800

    MongoDB set up

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import chatRouter from './routes/chat.route.js';
 
 dotenv.config(); // Validate the .env file.
+mongoose.connect(process.env.MONGO).then(() => console.log('Connect to MongoDB!')).catch(err => console.log(err)); // Connect the server to MongoDB.
 const app = express(); // This creates the server.
 app.use(express.json()); // This allows the server to accept requests with a JSON body.
 // app.use(cors());

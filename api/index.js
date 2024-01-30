bd42425db99da541a5fcf2205e95ac00415af703
LinLinHayashi <140152128+LinLinHayashi@users.noreply.github.com>
2024-01-29 18:31:40 -0800

    client sign-up functionality created

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import chatRouter from './routes/chat.route.js';
 
 dotenv.config(); // Validate the .env file.
 
-mongoose.connect(process.env.MONGO).then(() => console.log('Connect to MongoDB!')).catch(err => console.log(err)); // Connect the server to MongoDB.
+mongoose.connect(process.env.MONGO).then(() => console.log('Connected to MongoDB!')).catch(err => console.log(err)); // Connect the server to MongoDB.
 
 const app = express(); // This creates the server.
 
@@ -22,3 +22,14 @@ app.listen(8000, () => {
 // This is how we call an API router.
 app.use('/api/auth', authRouter);
 app.use('/api/chat', chatRouter);
+
+// The middleware for error handling.
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false, // If there is an error, "success" will be set to false.
+    statusCode,
+    message,
+  });
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,69 @@
 import "../styles/SignUp.css";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // This prevents refreshing the page when the form is submitted.
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      // If there is an error, as per the middleware for error handling we created in "index.js" in the backend.
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return; // End "handleSubmit" function as we have an error.
+      }
+
+      // If we are here, then we are successfully signed up.
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      // We use "try/catch" here to handle errors NOT defined in the backend.
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="sign-up">
       <h1>SIGN UP</h1>
-      <form className="sign-up-input-container">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button>SIGN UP</button>
+      <form className="sign-up-input-container" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading}>{loading ? "LOADING..." : "SIGN UP"}</button>
       </form>
       <div className="sign-in-info">
         <p>Have an account?</p>
@@ -16,6 +71,7 @@ export default function SignUp() {
           <span>Sign In</span>
         </Link>
       </div>
+      {error && <p className="error">{error}</p>}
     </div>
   );
 }
